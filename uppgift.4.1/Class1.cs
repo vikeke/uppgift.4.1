3be29f6f4964807bedef609ada6e3ce9a0c84705
vikeke <95035781+vikeke@users.noreply.github.com>
2022-11-11 09:26:38 +0100

    Add project files.

---
+++ uppgift.4.1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift._4._1
+{
+    public class Djur
+    {
+        private string namn;
+        private int older;
+        public string getNamn()
+        {
+            return this.namn;
+        }
+        public void setNamn(string name)
+        {
+            this.namn = namn;
+        }
+        public class katt : Djur
+        {
+            private int tassStorlek;
+            public void settassStorlek(int tassStorlek)
+            {
+                this.tassStorlek = tassStorlek;
+            }
+            public int gettassStorlek()
+            {
+                return this.tassStorlek;
+            }
+            public void jama()
+            {
+                Console.WriteLine("meow");
+            }
+        }
+    }
+}
---
+++ uppgift.4.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using uppgift._4._1;
+
+void Djur()
+{
+    //denna funktion användet get och set
+    Djur.katt katt = new Djur.katt();
+    katt.settassStorlek(12);
+    katt.setNamn("lilly");
+    Console.WriteLine("name: " + katt.getNamn());
+    Console.WriteLine("Tass Storlek: " + katt.gettassStorlek());
+    katt.jama();
+}
+
+Djur();
